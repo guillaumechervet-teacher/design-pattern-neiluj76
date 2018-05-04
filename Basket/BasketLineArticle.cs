29f3bcd7229799c61ea80bf53eba6130ae56ab3d
neiluj76 <julien.petit@epsi.fr>
2018-05-04 16:16:03 +0200

    Premier commit

---
+++ Basket/BasketLineArticle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket
+{
+    public struct BasketLineArticle
+    {
+        public string Id { get; set; }
+        public int Number { get; set; }
+        public string Label { get; set; }
+    }
+}
