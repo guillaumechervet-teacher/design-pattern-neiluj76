f4e6722d761a9d4868a1484fcc3c9d5d16864bdb
neiluj76 <julien.petit@epsi.fr>
2018-06-13 15:12:09 +0200

    Modifications

---
+++ Basket/OrientedObject/Domain/ArticleBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.OrientedObject.Domain
+{
+    public abstract class ArticleBase
+    {
+        public ArticleBase(string id, int price)
+        {
+            Id = id;
+            Price = price;
+        }
+        public int Price { get; }
+        public string Id { get; }
+        public abstract int CalculateAmout();
+    }
+}
---
+++ Basket/OrientedObject/Domain/ArticleFood.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.OrientedObject.Domain
+{
+    public class ArticleFood : ArticleBase
+    {
+        public ArticleFood(string id, int price) : base(id, price)
+        {
+        }
+        public override int CalculateAmout()
+        {
+            return Price * 100 + Price * 12;
+        }
+    }
+}
